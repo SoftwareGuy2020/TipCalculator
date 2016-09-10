56628717fa1a7fb482640f45f1c84b14091abe58
Travis <twm.direct@gmail.com>
2016-09-10 16:08:27 -0700

    Added translation for Portuguese(Brazil). Added javadoc for RestaurantBill class and MainActivity class. Fixed bug with EditText window.

--- app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/tipcalculator/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/tipcalculator/MainActivity.java
@@ -8,6 +8,14 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import java.util.Currency;
+import java.util.Locale;
+
+/**
+ * Controller for Tip Calculator.
+ * Inflates the GUI, creates an instance of RestaurantBill, and
+ * manages input/output of the Tip Calculator Application.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private TextView amountTextView;
@@ -16,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView totalTextView;
     private EditText amountEditText;
     private SeekBar percentSeekBar;
-
+    private String currencySymbol = Currency.getInstance(Locale.getDefault()).getSymbol();
     private RestaurantBill currentBill = new RestaurantBill();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,26 +40,61 @@ public class MainActivity extends AppCompatActivity {
         amountEditText = (EditText) findViewById(R.id.amountEditText);
         percentSeekBar = (SeekBar) findViewById(R.id.percentSeekBar);
 
+        // Sets the percentage of the RestaurantBill if it is not set.
+        currentBill.setTipPercent((double) percentSeekBar.getProgress() / 100.0);
+
         amountEditText.addTextChangedListener(amountTextChangeListener);
+        percentSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                percentTextView.setText(Integer.toString(progress).concat("%"));
+                currentBill.setTipPercent((double) progress / 100.0 );
+                tipTextView.setText(String.format(Locale.getDefault(), "%s%.2f",
+                        currencySymbol, currentBill.getTipAmount()));
+                totalTextView.setText(String.format(Locale.getDefault(), "%s%.2f",
+                        currencySymbol, currentBill.getTotalAmount()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // Do nothing
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // Do nothing
+            }
+        });
     }
 
     private final TextWatcher amountTextChangeListener = new TextWatcher() {
         @Override
         public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
+            // do nothing
         }
 
         @Override
         public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
             try {
-                double amount = Double.parseDouble(charSequence.toString());
-                amount /= 100;
-                currentBill.setAmount(amount);
-                amountTextView.setText(Double.toString(currentBill.getAmount()));
 
-            }
-            catch (NumberFormatException ex) {
+
+                double amount = charSequence.length() > 0 ?
+                        Double.parseDouble(charSequence.toString()) : 0.0;
+                amount /= 100.0;
+
+                currentBill.setAmount(amount);
+                amountTextView.setText(String.format(Locale.getDefault(), "%s%.2f",
+                       currencySymbol, currentBill.getAmount()));
+
+                tipTextView.setText(String.format(Locale.getDefault(), "%s%.2f",
+                        currencySymbol, currentBill.getTipAmount()));
+                totalTextView.setText(String.format(Locale.getDefault(), "%s%.2f",
+                        currencySymbol, currentBill.getTotalAmount()));
+
+
+            } catch (NumberFormatException ex) {
                 amountEditText.setText("");
             }
         }
--- app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/tipcalculator/RestaurantBill.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/tmorrissey1/tipcalculator/RestaurantBill.java
@@ -1,21 +1,42 @@
 package edu.orangecoastcollege.cs273.tmorrissey1.tipcalculator;
 
 /**
+ * Represents a restaurant bill
  * Created by tmorrissey1 on 9/8/2016.
  */
 public class RestaurantBill {
-
+    /**
+     * Decimal amount of this RestaurantBill before tip.
+     */
     private double mAmount;
+    /**
+     * Tip percentage of this RestaurantBill in decimal form.
+     */
     private double mTipPercent;
+    /**
+     * Tip amount of this RestaurantBill in decimal form.
+     */
     private double mTipAmount;
+    /**
+     * Decimal amount of this RestaurantBill after tip.
+     */
     private double mTotalAmount;
 
+    /**
+     * Constructor for RestaurantBill. Creates a new RestaurantBill with given amount and tip
+     * percent.
+     * @param amount This RestaurantBill's amount.
+     * @param TipPercent percentage of tip.
+     */
     public RestaurantBill(double amount, double TipPercent) {
         this.mAmount = amount;
         this.mTipPercent = TipPercent;
         recalculateAmounts();
     }
 
+    /**
+     * Default constructor for RestaurantBill.
+     */
     public RestaurantBill() {
         mTipAmount = 0.0;
         mAmount = 0.0;
@@ -23,32 +44,61 @@ public class RestaurantBill {
         mTipPercent = 0.0;
     }
 
+    /**
+     * Gets the monetary amount of this RestaurantBill before the tip.
+     * @return This RestaurantBill's amount before tip.
+     */
     public double getAmount() {
         return mAmount;
     }
 
+    /**
+     * Sets the decimal amount of this RestaurantBill before tip.
+     * @param mAmount New amount of this RestaurantBill.
+     */
     public void setAmount(double mAmount) {
         this.mAmount = mAmount;
         recalculateAmounts();
     }
 
+    /**
+     * Gets the tip percentage of this RestaurantBill in decimal format.
+     * @return Tip Percentage in decimal format of this RestaurantBill.
+     */
     public double getTipPercent() {
         return mTipPercent;
     }
 
+    /**
+     * Sets the tip percentage of this RestaurantBill and recalculates both the tip amount and the
+     * total amount.
+     * @param mTipPercent Tip percentage of this RestaurantBill in decimal format.
+     *                    (Example: 10% as 0.10).
+     */
     public void setTipPercent(double mTipPercent) {
         this.mTipPercent = mTipPercent;
         recalculateAmounts();
     }
 
+    /**
+     * Gets the decimal amount of this RestaurantBill's tip.
+     * @return decimal amount of this RestaurantBill's tip.
+     */
     public double getTipAmount() {
         return mTipAmount;
     }
 
+    /**
+     * Gets the total amount of this RestaurantBill in decimal form.
+     * @return Total amount of this RestaurantBill in decimal form.
+     */
     public double getTotalAmount() {
         return mTotalAmount;
     }
 
+    /**
+     *  Recalculates this RestaurantBill's amounts of the tip and total.
+     */
     private void recalculateAmounts() {
         mTipAmount = mAmount * mTipPercent;
         mTotalAmount = mAmount + mTipAmount;
